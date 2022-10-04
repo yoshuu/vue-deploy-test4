7ec22db9e942625bbb8db509f61b43c9e2188bf2
yoshuu <appcs342@gmail.com>
2022-10-04 18:14:49 +0800

    feat: modify vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.NODE_ENV === "production" ? "/<REPO>/" : "./",
+  base: process.env.NODE_ENV === "production" ? "/vue-deploy-test4/" : "./",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
